6657597661deef72cc0489bf97b15d6afc43b0e4
Georginio-prod <etonameklou6@gmail.com>
2026-01-31 14:09:16 +0000

    Add Plateforme component and iPhone mockup to landing page

--- Orga-landingPage/src/App.vue
+++ Orga-landingPage/src/App.vue
@@ -2,12 +2,14 @@
 import Navbar from './components/Navbar.vue'
 import HeroSection from './components/HeroSection.vue'
 import Solution from './components/Solution.vue'
+import Plateforme from './components/Plateforme.vue'
 </script>
 
 <template>
   <Navbar />
   <HeroSection />
   <Solution />
+  <Plateforme />
 </template>
 
 <style scoped></style>
